9f195d2e1cacc2725f0c2f4910dadae6a36ba78e
shivam <shivam_0217@rediffmail.com>
2022-06-09 00:55:09 +0530

    added basic functionality

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+
+interface IData {
+    void parse(String[] in);
+}
+
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         ProcessInput in = new ProcessInput();
         do {
             String input = sc.nextLine();
@@ -15,60 +20,59 @@ public class Main {
 
 }
 
-class ProcessInput{
+class ProcessInput {
+    public final String err = "I have no idea what you are talking about";
     IData material;
     IData roman;
     ProcessOutput out;
-    public final String err = "I have no idea what you are talking about";
-    public void inputProcessor(String data){
+
+    public void inputProcessor(String data) {
         String[] in = data.split("\\s+");
 
-        if (in.length == 3 && in[1].equalsIgnoreCase("is")){
+        if (in.length == 3 && in[1].equalsIgnoreCase("is")) {
             if (roman == null)
                 roman = new Roman();
             roman.parse(in);
-        }else if (data. toLowerCase().endsWith("credits")) {
+        } else if (data.toLowerCase().endsWith("credits")) {
             if (material == null)
                 material = new Materials();
             material.parse(in);
-        }else if (data.toLowerCase().endsWith("?")){
-            if (out == null){
+        } else if (data.toLowerCase().endsWith("?")) {
+            if (out == null) {
                 out = new ProcessOutput();
             }
-            if (data.toLowerCase().startsWith("how much is")){
-                if ((!out.howMuch(in))){
+            if (data.toLowerCase().startsWith("how much is")) {
+                if ((!out.howMuch(in))) {
                     System.out.println(err);
                 }
-            }else if (data.toLowerCase().startsWith("how many credits")){
-                if (!(out.howMany(in))){
+            } else if (data.toLowerCase().startsWith("how many credits")) {
+                if (!(out.howMany(in))) {
                     System.out.println(err);
                 }
-            }else {
+            } else {
                 System.out.println(err);
             }
-        }else{
-            System.out.println("\n"+err);
+        } else {
+            System.out.println("\n" + err);
         }
     }
 }
 
-interface IData{
-    void parse(String[] in);
-}
+class Roman implements IData {
+    public static Map<String, String> romanTrans = new HashMap<>();
 
-class Roman implements IData{
-    public static Map<String,String> romanTrans = new HashMap<>();
     @Override
     public void parse(String[] in) {
-        romanTrans.put(in[0],in[2]);
+        romanTrans.put(in[0], in[2]);
     }
-    public int getValue(String[] in,int start, int end){
+
+    public int getValue(String[] in, int start, int end) {
         int sum = 0;
-        for (int i = start; i <= end; i++){
+        for (int i = start; i <= end; i++) {
             char nextChar = ' ';
             char rChar = romanTrans.get(in[i]).charAt(0);
-            if (i != end){
-                nextChar = romanTrans.get(in[i+1]).charAt(0);
+            if (i != end) {
+                nextChar = romanTrans.get(in[i + 1]).charAt(0);
             }
             switch (rChar) {
                 case 'M':
@@ -113,50 +117,53 @@ class Roman implements IData{
     }
 }
 
-class Materials implements IData{
-    public static Map<String,Double> materialsValue = new HashMap<>();
+class Materials implements IData {
+    public static Map<String, Double> materialsValue = new HashMap<>();
+
     @Override
     public void parse(String[] in) {
         int len = in.length;
-        double value = Double.parseDouble(in[len-2]);
+        double value = Double.parseDouble(in[len - 2]);
         Roman roman = new Roman();
-        int qty = roman.getValue(in,0,len-5);
-        materialsValue.put(in[len-4],value/qty);
+        int qty = roman.getValue(in, 0, len - 5);
+        materialsValue.put(in[len - 4], value / qty);
     }
-    public double getValue(String str){
-        if (materialsValue.containsKey(str)){
+
+    public double getValue(String str) {
+        if (materialsValue.containsKey(str)) {
             return materialsValue.get(str);
-        }
-        else
+        } else
             return -1;
     }
 }
 
-class ProcessOutput{
+class ProcessOutput {
     Roman roman = new Roman();
-    public boolean howMuch(String[] in){
-        int val = roman.getValue(in,3,in.length - 2);
 
-        StringBuffer sb =new StringBuffer();
-        for (int i = 3;i < in.length - 1; i++){
+    public boolean howMuch(String[] in) {
+        int val = roman.getValue(in, 3, in.length - 2);
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 3; i < in.length - 1; i++) {
             sb.append(in[i]).append(" ");
         }
         sb.append("is ").append(val);
         System.out.println(sb);
         return true;
     }
-    public boolean howMany(String[] in){
-        int qty = roman.getValue(in,4,in.length - 3);
-        StringBuffer sb =new StringBuffer();
-        for (int i = 4;i < in.length - 1; i++){
+
+    public boolean howMany(String[] in) {
+        int qty = roman.getValue(in, 4, in.length - 3);
+        StringBuffer sb = new StringBuffer();
+        for (int i = 4; i < in.length - 1; i++) {
             sb.append(in[i]).append(" ");
         }
         Materials materials = new Materials();
-        double val = materials.getValue(in[in.length-2]);
-        if (val <= 0){
+        double val = materials.getValue(in[in.length - 2]);
+        if (val <= 0) {
             return false;
         }
-        sb.append("is ").append((qty*val)).append(" Credits");
+        sb.append("is ").append((qty * val)).append(" Credits");
         System.out.println(sb);
         return true;
     }
